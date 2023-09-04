f28634d9e54fd86a1fb4a008b4a403fa77ebf6af
etiennedibi <46790447+etiennedibi@users.noreply.github.com>
2023-09-04 23:32:23 +0000

    Update main.js to hover icore domain name

--- src/main.js
+++ src/main.js
@@ -16,7 +16,7 @@ Vue.config.productionTip = false;
 Vue.use(VueAxios, axios);
 
 
-axios.defaults.baseURL = "https://synthesia.globalbch.cloud";
+axios.defaults.baseURL = "https://coiffure.startup-invest.online";
 // pour garder l'autorisation dans le header, lorsqu'il y a rechargement de page. parcequ'au recharegment
 // le header est suprimé, mais pas le localeStorage  "https://mino-api.blooraid-consortium.com"
 const token = localStorage.getItem("user-token");

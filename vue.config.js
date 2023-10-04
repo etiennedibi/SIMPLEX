5d05c24c4366c7ec2a10a20e46b99d85d5f63eab
etiennedibi <etiennedibi@gmail.com>
2023-10-04 22:50:49 +0000

    desktop icon

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,29 @@
 module.exports = {
   transpileDependencies: ["vuetify"],
 
+  pluginOptions: {
+    electronBuilder: {
+        builderOptions: {
+            productName: "Simplex",
+            appId: 'simplex.com',
+            win: {
+                "target": [
+                    "nsis"
+                ],
+              icon: 'public/simplexLogo.png',
+              "requestedExecutionLevel": "requireAdministrator"
+            },
+            "nsis": {
+                "installerIcon": "public/simplexfav.ico",
+                "uninstallerIcon": "public/simplexfav.ico",
+                "uninstallDisplayName": "CPU Monitor",
+                "license": "license.txt",
+                "oneClick": false,
+                "allowToChangeInstallationDirectory": true
+            }
+        },
+    },
+  },
   // devServer: {
   //   proxy: 'https://mino-api.blooraid-consortium.com/'
   // }
